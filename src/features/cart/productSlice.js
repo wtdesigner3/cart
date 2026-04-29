c00506ae50263d94b9d50d8c944b99c004915ffc
Your Name <you@example.com>
2026-04-29 15:21:54 +0530

    fix API URL for production

--- src/features/cart/productSlice.js
+++ src/features/cart/productSlice.js
@@ -1,7 +1,7 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
 import axios from 'axios'
 
-const API_BASE = 'http://localhost:5000/api'
+const API_BASE = 'import.meta.env.VITE_API_URL'
 
 export const fetchProduct = createAsyncThunk('product/fetchProduct', async () => {
   const response = await axios.get(`${API_BASE}/products`)
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_BASE = 'http://localhost:5000/api'
+const API_BASE = 'import.meta.env.VITE_API_URL'
 const api = axios.create({
   baseURL: API_BASE,
 })
