a007ff18144e47f9851a786eff6fd322b479702c
Your Name <you@example.com>
2026-04-29 16:12:02 +0530

    change-2

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const API_BASE = (import.meta.env.VITE_API_URL || '').replace(/\/$/, '')
 const api = axios.create({
-  baseURL: API_BASE,
+  baseURL: `${API_BASE}/api`,
 })
 
 export const authHeaders = (token) => ({
